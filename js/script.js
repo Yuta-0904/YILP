dba1a2c5aedb8457149a19ddbeb026ad653bf602
Nununta <yutayuki09040827@gmail.com>
2021-12-12 16:32:14 +0900

    アニメーション追加完了

--- js/script.js
+++ js/script.js
@@ -4,4 +4,16 @@ $(function () {
     $("#global-nav").toggleClass("open");
     $(".toggle").toggleClass("open");
   });
+
+  const fadeIn = $(".fade-in");
+  $(window).scroll(function () {
+    $(fadeIn).each(function () {
+      var offset = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > offset - windowHeight + 150) {
+        $(this).addClass("scroll-in");
+      }
+    });
+  });
 });
